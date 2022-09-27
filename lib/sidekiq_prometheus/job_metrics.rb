4972eb20a2c1fbccf4e960833a9fa7c72cb198d5
Lukas Eklund <leklund@gmail.com>
2022-09-27 12:43:13 -0500

    Update lib/sidekiq_prometheus/job_metrics.rb

--- lib/sidekiq_prometheus/job_metrics.rb
+++ lib/sidekiq_prometheus/job_metrics.rb
@@ -30,7 +30,7 @@ class SidekiqPrometheus::JobMetrics
 
       result
     rescue StandardError => e
-      if e.class.to_s == "Sidekiq::Limiter::OverLimit"
+      if e.class.to_s == 'Sidekiq::Limiter::OverLimit'
         registry[:sidekiq_job_over_limit].increment(labels: labels)
       else
         err_label = { :error_class => e.class.to_s }
